31ba74f2cde898f986925a6252533835e8752402
Bala Subrahmanyam Varanasi <balu@agiliq.com>
2013-03-29 01:29:42 +0530

    Listing All Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,10 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     @posts = Post.all
-
+   
     respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @posts }
+      format.html  # index.html.erb
+      format.json  { render :json => @posts }
     end
   end
 
