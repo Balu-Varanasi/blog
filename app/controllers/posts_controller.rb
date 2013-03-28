112c8e2d80c2210939f1e3cde8508ee6043ff05d
Bala Subrahmanyam Varanasi <balu@agiliq.com>
2013-03-29 02:41:37 +0530

    fixed errors in PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,15 @@
 class PostsController < ApplicationController
- 
-  http_basic_authenticate_with :name => "dhh", :password => "secret", :except => [:index, :show]
- 
+
+  http_basic_authenticate_with :name => "dhh", :password => "secret", :except => :index
+
   # GET /posts
   # GET /posts.json
   def index
     @posts = Post.all
 
     respond_to do |format|
-   
-    respond_to do |format|
-      format.html  # index.html.erb
-      format.json  { render :json => @posts }
+      format.html # index.html.erb
+      format.json { render json: @posts }
     end
   end
 
